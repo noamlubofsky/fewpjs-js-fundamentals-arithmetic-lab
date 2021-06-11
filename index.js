b748692a7fb805b83ab98bbcb480c8896d266c74
github username <github email address>
2021-06-10 21:53:52 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = (num1 * num2)
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 18
+const num4 = 22
+const mod = (num4 % num3)
+
+const max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
